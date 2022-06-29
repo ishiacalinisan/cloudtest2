907603879d548ba1f6b63b3364e0bab55791980b
NickDeitmers <71077922+NickDeitmers@users.noreply.github.com>
2022-06-29 16:44:12 +0200

    switch back older version

--- src/components/ShopProviders/ShopProviders.js
+++ src/components/ShopProviders/ShopProviders.js
@@ -40,6 +40,68 @@ class ShopProviders extends Component {
     return (
       <div className="shop-products-area">
 
+        {/* shop grid products */}
+        <div
+          className={`shop-grid-products-wrapper space-mb-m--20 ${
+            gridActivate ? "d-block" : "d-none"
+          }`}
+        >
+          <div className="container">
+            <div className="row">
+              {products &&
+                products.map(single => {
+                  const wishlistItem = wishlistItems.filter(
+                    wishlistItem => wishlistItem.id === single.id
+                  )[0];
+                  return (
+                    <div className="col-6" key={single.id}>
+                      <div className="grid-product space-mb--20">
+                        <div className="grid-product__image">
+                          <Link
+                            to={
+                              process.env.PUBLIC_URL + `/services/${single.id}`
+                            }
+                          >
+                            <img
+                              src={process.env.PUBLIC_URL + single.image[0]}
+                              className="img-fluid"
+                              alt=""
+                            />
+                          </Link>
+                        </div>
+                        <div className="grid-product__content">
+                          <h3 className="title">
+                            <Link
+                              to={
+                                process.env.PUBLIC_URL + `/services/${single.id}`
+                              }
+                            >
+                              {single.name}
+                            </Link>
+                          </h3>
+                          <div className="rating">
+                              <img
+                                  src={process.env.PUBLIC_URL + single.rating_stars}
+                                  className="img-fluid"
+                                  alt=""
+                              />
+                          </div>
+                          <span className="category">
+                            {single.category.map((item, index, arr) => {
+                              return (
+                                item + (index !== arr.length - 1 ? ", " : "")
+                              );
+                            })}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
+        </div>
+
         {/* shop list products */}
         <div
           className={`shop-list-products-wrapper ${
@@ -91,7 +153,7 @@ class ShopProviders extends Component {
                         {single.address}
                       </span>
 
-                  </div> 
+                  </div>
                 </div>
               );
             })}
